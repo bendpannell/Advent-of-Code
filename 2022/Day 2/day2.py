1e6b4bfdadf04bc0ef85497d64d642e4f3ae7972
Ben Pannell <ben.pannell@myollie.com>
2022-12-02 10:42:34 -0700

    Refactor to show both parts

--- 2022/Day 2/day2.py
+++ 2022/Day 2/day2.py
@@ -12,20 +12,20 @@ input = np.loadtxt('input.txt', dtype = 'str')
 
 mylist = input
 
-# Part 1 dicts
-# win = {"A": "Y",
-#        "B": "Z",
-#        "C": "X"}
+# Part 1 Mapping
+win = {"A": "Y",
+        "B": "Z",
+        "C": "X"}
 
-# draw = {"A": "X",
-#         "B": "Y",
-#         "C": "Z"}
+draw = {"A": "X",
+        "B": "Y",
+        "C": "Z"}
 
-# lose = {"A": "Z",
-#         "B": "X",
-#         "C": "Y"}
+lose = {"A": "Z",
+        "B": "X",
+        "C": "Y"}
 
-# Part 2 dicts
+# Part 2 Mapping
 rock = {"A": "Y",
         "B": "X",
         "C": "Z"}
@@ -38,38 +38,53 @@ scissors = {"A": "X",
             "B": "Z",
             "C": "Y"}
 
-results = []
 
-def win_value(sign):
-    if sign == 'X':
-        return 0
-    elif sign == 'Y':
-        return 3
-    elif sign == 'Z':
-        return 6
+
+def win_value(part, sign):
     
-
-for round in mylist:
-    # Part 1
-    # if round[0] in win and round[1] == win[round[0]]:
-    #     results.append(6 + win_value(round[1]))
-
-    # elif round[0] in draw and round[1] == draw[round[0]]:
-    #     results.append(3 + win_value(round[1]))
-
-    # elif round[0] in lose and round[1] == lose[round[0]]:
-    #     results.append(win_value(round[1]))
+    if part == 1:
+        map = {"X": 1,
+               "Y": 2,
+               "Z": 3}
+        
+    elif part == 2:
+        map = {"X": 0,
+               "Y": 3,
+               "Z": 6}
     
-    #Part 2
-    if round[0] in rock and round[1] == rock[round[0]]:
-        results.append(1 + win_value(round[1]))
+    if sign in map:
+        return map[sign]
+    
+    
+def main(part):
+    results = []
+    
+    for round in mylist:
+        # Part 1
+        if part == 1:
+            if round[0] in win and round[1] == win[round[0]]:
+                results.append(6 + win_value(part, round[1]))
         
-    elif round[0] in paper and round[1] == paper[round[0]]:
-        results.append(2 + win_value(round[1]))
+            elif round[0] in draw and round[1] == draw[round[0]]:
+                results.append(3 + win_value(part, round[1]))
         
-    elif round[0] in scissors and round[1] == scissors[round[0]]:
-        results.append(3 + win_value(round[1]))
-        
-# print("Part 1:", sum(results))
-print("Part 2:", sum(results))
+            elif round[0] in lose and round[1] == lose[round[0]]:
+                results.append(win_value(part, round[1]))
+                
+                
+        elif part == 2:
+            #Part 2
+            if round[0] in rock and round[1] == rock[round[0]]:
+                results.append(1 + win_value(part, round[1]))
+                
+            elif round[0] in paper and round[1] == paper[round[0]]:
+                results.append(2 + win_value(part, round[1]))
+                
+            elif round[0] in scissors and round[1] == scissors[round[0]]:
+                results.append(3 + win_value(part, round[1]))
+            
+    print(f"Part {part}: {sum(results)}")
+    
+main(1)
+main(2)
 
